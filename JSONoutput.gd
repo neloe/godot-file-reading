e7cabfd08ba3b19ba1a52003158099f70891bc3b
Nathan Eloe <powerofazure@gmail.com>
2021-04-29 14:53:09 -0500

    Demo how to do dynamic buttons

--- JSONoutput.gd
+++ JSONoutput.gd
@@ -24,14 +24,28 @@ func load_trivia():
 	questions = JSON.parse(file.get_as_text())
 	file.close()
 
+func correct_answer():
+	print('yay')
+	
+func wrong_answer():
+	print('boo')
+
 func show_trivia(idx):
 	qtext.text = questions.result[idx]['question']
+	var answer = questions.result[idx]['answer']
 	# you could do some cool things with flat buttons and shuffle the correct answers here, up to you!
 	# the thing to do might be to make an array of clickable buttons with the texts, hook up the buttons to the 
 	# correct success/failure functions, then shuffle them.  I'm just setting labels
 	questions.result[idx]['answers'].shuffle()
 	for i in range(4):
 		atexts[i].text = questions.result[idx]['answers'][i]
+		for method in atexts[i].get_signal_connection_list('pressed'):
+			atexts[i].disconnect('pressed', self, method['method'])
+		if questions.result[idx]['answers'][i] == answer:
+			atexts[i].connect('pressed', self, 'correct_answer')
+		else:
+			atexts[i].connect('pressed', self, 'wrong_answer')
+		
 
 
 func _on_Next_pressed():
